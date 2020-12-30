ed033242681c27834edf8512ef540584f74f0a40
Michel <michel.p.n@hotmail.com>
2020-12-29 23:52:31 -0300

    ..

--- lib/diametro.dart
+++ lib/diametro.dart
@@ -17,14 +17,14 @@ class _diametroState extends State<diametro> {
       raiz = 1.7,
       cos = 0.85,
       kva,
-      total,
+      total = 0,
       correnteValue,
       resistenciaValue,
       dVporc;
-  double i;
+  double i, y = 0;
 
   double fp = 1.25;
-//double r1=0.33, r2=0.53,r3=0.84,r4=1.34,r5=2.13,r6=3.39,r7=5.38,r8=8.57;
+
   String _infoText = '', x;
   GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
@@ -36,12 +36,11 @@ class _diametroState extends State<diametro> {
       double distanciaValue = double.tryParse(distanciaController.text);
 
       kva = wattsValue * fp;
+      print("kva $kva");
+      correnteValue = (kva / (raiz * tensaoValue * cos));
+      print("corrente $correnteValue");
+      Map<double, dynamic> lista = Map();
 
-      correnteValue = ((raiz * tensaoValue * cos) / kva);
-print("corrente $correnteValue");
-Map< double, dynamic> lista= Map();
-
-      
       lista[0] = 8.57;
       lista[1] = 5.38;
       lista[2] = 3.39;
@@ -51,42 +50,43 @@ Map< double, dynamic> lista= Map();
       lista[6] = 0.53;
       lista[7] = 0.33;
       lista[8] = 0.26;
-      print("lista, $lista");
 
       do {
-         
-        if (correnteValue <= 30) {
-          lista[i] = lista[0];
-         
-        } else if (correnteValue > 30 && correnteValue <= 40) {
-          lista[i] = lista[1];
-         
-        } else if (correnteValue > 40 && correnteValue <= 55) {
-          lista[i] = lista[2];
-        } else if (correnteValue > 55 && correnteValue <= 70) {
-          lista[i] = lista[3];
-        } else if (correnteValue > 70 && correnteValue <= 100) {
-          lista[i] = lista[4];
-        } else if (correnteValue > 100 && correnteValue <= 130) {
-          lista[i] = lista[5];
-        } else if (correnteValue > 130 && correnteValue <= 175) {
-          lista[i] = lista[6];
-        } else if (correnteValue > 175 && correnteValue <= 275) {
-          lista[i] = lista[7];
-        } else if (correnteValue > 275) {
-          lista[i] = lista[8];
-        }
-print("lista$lista[i]");
-        dV = raiz * lista[i] * distanciaValue * correnteValue * cos;
-print("dV $dV");
+        if (lista[y] > 0) {
+          lista[y]= y++;
+        } 
+          if (correnteValue <= 30) {
+            lista[y] = lista[0];
+          } else if (correnteValue > 30 && correnteValue <= 40) {
+            lista[y] = lista[1];
+          } else if (correnteValue > 40 && correnteValue <= 55) {
+            lista[y] = lista[2];
+          } else if (correnteValue > 55 && correnteValue <= 70) {
+            lista[y] = lista[3];
+          } else if (correnteValue > 70 && correnteValue <= 100) {
+            lista[y] = lista[4];
+          } else if (correnteValue > 100 && correnteValue <= 130) {
+            lista[y] = lista[5];
+          } else if (correnteValue > 130 && correnteValue <= 175) {
+            lista[y] = lista[6];
+          } else if (correnteValue > 175 && correnteValue <= 275) {
+            lista[y] = lista[7];
+          } else if (correnteValue > 275) {
+            lista[y] = lista[8];
+          }
+        
+        print("lista $lista[y]");
+        dV = raiz * lista[y] * distanciaValue * correnteValue * cos;
+        print("dV $dV");
         dVporc = ((dV / tensaoValue) * 100);
-       }while (lista[i] > 5 && lista[i]++);
+        
+        print("Porcentagem $dVporc");
+      } while (dVporc > 5);
 
-      dV = raiz * lista[i] * distanciaValue * correnteValue * cos;
-      print(dV);
-    
-    });}
-  
+      dV = raiz * lista[y] * distanciaValue * correnteValue * cos;
+      print("dV Valor final mostrado $dV");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
